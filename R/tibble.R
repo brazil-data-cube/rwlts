f6ee1ceaad79c283ada2e552c3cd80525589c02d
Felipe <lipecaso@gmail.com>
2020-11-12 00:32:20 -0300

    Add an index to create tibble

--- R/tibble.R
+++ R/tibble.R
@@ -5,6 +5,16 @@
 #' @return
 #'
 #'
-.build_result_tibble <- function(result) {
-  do.call(rbind, sapply(result$trajectory, tibble::as_tibble, simplify = FALSE))
+.build_wlts_tb <- function(result, index) {
+
+  # transform to tibble the result requested
+  wlts_tb <- do.call(rbind, sapply(result$trajectory,
+                                   tibble::as_tibble,
+                                   simplify = FALSE))
+
+  # add an index to each extracted point
+  if (!length(wlts_tb) == 0)
+    wlts_tb <- tibble::add_column(wlts_tb, point_id = index)
+
+  return(wlts_tb)
 }
