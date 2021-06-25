f58fc705320055940c47d48bcedeb6cc2dcc3807
Felipe <lipecaso@gmail.com>
2021-06-25 04:47:51 +0000

    add external data as trajectory example

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' @title Trajectory data from Mato Grosso of collection Map Biomas 5
+#' @docType data
+#' @name mt_500_mapbiomas_cerrado
+#' @keywords datasets
+#' @description
+#' A dataset containing a wlts tibble, with result and query attributes.
+#'
+#' @format
+#' A \code{wlts} tibble with 5.5K samples. A \code{wlts} tibble contains
+#' the following meta-data:
+#' query (a NULL value),
+#' result (a tibble with trajectory information.).
+#'
+#' @usage data("mt_500_mapbiomas_cerrado")
+#'
+NULL
